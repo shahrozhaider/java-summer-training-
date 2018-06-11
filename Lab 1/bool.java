6a718609ea0532fdfdfa25f6ce50b5e565124b80
shahrozhaider <40165014+shahrozhaider@users.noreply.github.com>
2018-06-11 14:36:03 +0530

    Lab 1 !

---
+++ Lab 1/bool.java
@@ -0,0 +1,11 @@
+class Test{
+   public static void main(String args[]) throws Exception{
+      boolean bool1 = true;
+      boolean bool2 = false;
+      boolean bool = (25==(100/4));
+     
+      System.out.println(bool1);
+      System.out.println(bool2);
+      System.out.println(bool);
+   }
+}
